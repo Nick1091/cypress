360e2dd22353e8a0fe84917dc7cbdee2dc140898
Nick1091 <nick10.91@mail.ru>
2024-09-27 08:36:52 +0300

    fix bg

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { AuthForm } from './components/AuthForm'
 
 function App() {
   return (
-    <div style={{ background: '#ffefef99', width: '100%', height: '100%' }}>
+    <div>
       <AuthForm />
     </div>
   );
